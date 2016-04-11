4b3eea95f651d40500b3af65a426ab144e08b5a3
Matthew Marino <mmarino@jcmar.com>
2016-04-11 14:10:32 -0700

    fixed task name from sails-inject-gulp back to sails-linker-glp

--- templates/tasks-gulp/config/sails-linker-gulp.js
+++ templates/tasks-gulp/config/sails-linker-gulp.js
@@ -14,7 +14,7 @@
 module.exports = function(gulp, plugins, growl) {
 
     // Insert JS, CSS and template dev links into HTML files in the tmp assets folder
-    gulp.task('sails-inject-gulp:devAssets', function() {
+    gulp.task('sails-linker-gulp:devAssets', function() {
         // Read templates
         return gulp.src('.tmp/public/**/*.html')
             // Link the javaScript
@@ -52,11 +52,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('.tmp/public/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:devAssets task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:devAssets task complete' })));
     });
 
     // Insert JS, CSS and template dev links into HTML and EJS files in the views folder
-    gulp.task('sails-inject-gulp:devViews', function() {
+    gulp.task('sails-linker-gulp:devViews', function() {
         // Read templates
         return gulp.src(['views/**/*.html', 'views/**/*.ejs'])
             // Link the javaScript
@@ -94,11 +94,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('views/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:devViews task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:devViews task complete' })));
     });
 
     // Insert relative JS, CSS and template dev links into HTML files in the tmp assets folder
-    gulp.task('sails-inject-gulp:devAssetsRelative', function() {
+    gulp.task('sails-linker-gulp:devAssetsRelative', function() {
         // Read templates
         return gulp.src('.tmp/public/**/*.html')
             // Link the JavaScript
@@ -136,11 +136,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('.tmp/public/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:devAssetsRelative task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:devAssetsRelative task complete' })));
     });
 
     // Insert relative JS, CSS and template dev links into HTML and EJS files in the views folder
-    gulp.task('sails-inject-gulp:devViewsRelative', function() {
+    gulp.task('sails-linker-gulp:devViewsRelative', function() {
         // Read templates
         return gulp.src(['views/**/*.html', 'views/**/*.ejs'])
             // Link the javaScript
@@ -178,11 +178,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('views/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:devViewsRelative task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:devViewsRelative task complete' })));
     });
 
     // Insert JS, CSS and template production links into HTML files in the tmp assets folder
-    gulp.task('sails-inject-gulp:prodAssets', function() {
+    gulp.task('sails-linker-gulp:prodAssets', function() {
         // Read templates
         return gulp.src('.tmp/public/**/*.html')
             // Link the JavaScript
@@ -220,11 +220,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('.tmp/public/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:prodAssets task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:prodAssets task complete' })));
     });
 
     // Insert JS, CSS and template production links into HTML and EJS files in the views folder
-    gulp.task('sails-inject-gulp:prodViews', function() {
+    gulp.task('sails-linker-gulp:prodViews', function() {
         // Read templates
         return gulp.src(['views/**/*.html', 'views/**/*.ejs'])
             // Link the JavaScript
@@ -262,11 +262,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('views/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:prodViews task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:prodViews task complete' })));
     });
 
     // Insert relative JS, CSS and template production links into HTML files in the tmp assets folder
-    gulp.task('sails-inject-gulp:prodAssetsRelative', function() {
+    gulp.task('sails-linker-gulp:prodAssetsRelative', function() {
         // Read templates
         return gulp.src('.tmp/public/**/*.html')
             // Link the JavaScript
@@ -304,11 +304,11 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('.tmp/public/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:prodAssetsRelative task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:prodAssetsRelative task complete' })));
     });
 
     // Insert relative JS, CSS and template production links into HTML and EJS files in the views folder
-    gulp.task('sails-inject-gulp:prodViewsRelative', function() {
+    gulp.task('sails-linker-gulp:prodViewsRelative', function() {
         // Read templates
         return gulp.src(['views/**/*.html', 'views/**/*.ejs'])
             // Link the JavaScript
@@ -346,7 +346,7 @@ module.exports = function(gulp, plugins, growl) {
             )
             // Write modified files...
             .pipe(gulp.dest('views/'))
-            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-inject-gulp:prodViewsRelative task complete' })));
+            .pipe(plugins.if(growl, plugins.notify({ message: 'sails-linker-gulp:prodViewsRelative task complete' })));
     });
 
 };
